123b880e83a2696f1ea236f425a3588db2d01725
Diego Sousa <diegomsousaa@gmail.com>
2022-01-24 23:16:40 -0500

    Update q7-q11.js

--- q7-q11.js
+++ q7-q11.js
@@ -1,14 +1,32 @@
-const x = 6
+const x = 6;
 
 // 7. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" using without using arrow functions.
+function add2NumberToX(arg1, arg2) {
+  const sum = 0;
+  sum = arg1 + arg2 + x;
+  return sum;
+}
 
 // 8. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x", using arrow functions.
 
+const add2NumberToXArrowFunc = (arg1, arg2) => {
+  const sum = 0;
+  sum = arg1 + arg2 + x;
+  return sum;
+};
 // 9. Write a function that returns another function. (use arrow functions please)
 
+const returnAnotherFunc = () => {
+  const y = 10;
+
+  let add = (i) => i + y;
+
+  return add;
+};
+
+console.log(returnAnotherFunc()(5));
 
 // 10. given the following code:
-
 
 const getFunction = () => {
   const y = 5;
@@ -18,9 +36,12 @@ const getFunction = () => {
   return insideFunc;
 };
 
-console.log(getFunction()(2))
+console.log(getFunction()(2));
 
 // Explain why the function that returns from getFunction still has access to "y" if "y" is not a global variable.
+
+// Answer =============================================================================================
+// ***It still has access to Y value,  because Y belongs to scope of that callback function when  it was created****
 
 
 
